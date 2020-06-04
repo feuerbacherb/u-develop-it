34d9c107b972fd9fe3fe826ee520a1dcc7ca03c7
Brent F <feuerbacherb@gmail.com>
2020-06-03 19:55:47 -0500

    added routes for CRUD parties

--- server.js
+++ server.js
@@ -41,7 +41,6 @@ app.get('/api/candidates', (req, res) => {
    });
 });
 
-
 // GET a single candidate
 app.get('/api/candidate/:id', (req, res) => {
    const sql = `SELECT candidates.*, parties.name
@@ -105,6 +104,78 @@ app.post('/api/candidate', ({ body }, res) => {
       });
    });
 });
+
+// GET a list of all the parties
+app.get('/api/parties', (req, res) => {
+   const sql = `SELECT * FROM parties`;
+   const params = [];
+   db.all(sql, params, (err, rows) => {
+      if (err) {
+         res.status(500).json({ error: err.message });
+         return;
+      }
+      res.json({
+         message: 'success',
+         data: rows
+      });
+   });
+});
+
+// GET a single party based on id
+app.get(`/api/party/:id`, (req, res) => {
+   const sql = `SELECT * FROM parties WHERE id = ?`;
+   const params = [req.params.id];
+   db.get(sql, params, (err, row) => {
+      if (err) {
+         res.status(400).json({ error: err.message });
+         return;
+      }
+      res.json({
+         message: 'success',
+         data: row
+      });
+   });
+});
+
+// PUT a candidates allegiance to a different one
+app.put('/api/candidate/:id', (req, res) => {
+   const errors = inputCheck(req.body, 'party_id');
+
+   if (errors) {
+      res.status(400).json({ error: errors });
+      return;
+   }
+
+   const sql = `UPDATE candidates SET party_id = ? WHERE id = ?`;
+   const params = [req.body.party_id, req.params.id];
+
+   db.run(sql, params, function(err, result) {
+      if (err) {
+         res.status(400).json({ error: err.message });
+         return;
+      }
+      res.json({
+         message: 'success',
+         data: req.body,
+         changes: this.changes
+      });
+   });
+});
+
+//  DELETE a party based on id
+app.delete(`/api/party/:id`, (req, res) => {
+   const sql = `DELETE FROM parties WHERE id = ?`;
+   const params = [req.params.id];
+   db.run(sql, params, function(err, result) {
+      if (err) {
+         res.status(400).json({ error: res.message });
+         return;
+      }
+
+      res.json({ message: 'successfully deleted', changes: this.changes });
+   });
+});
+
 
 
 // default to handle any other request (Not Found) Catch all
